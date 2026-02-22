09b101ddd309fe97c00b7f44a3ad7f4049941c83
touhidul7 <touhidul2@hotmail.com>
2026-02-23 04:43:46 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "192.168.0.106",
-        port: "8000",
+        protocol: "https",
+        hostname: "https://brittopos.brittosoft.site",
+        // port: "8000",
         pathname: "/storage/**",
       },
     ],
